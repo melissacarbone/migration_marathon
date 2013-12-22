a5c974d210bcc6688cb343c992649f7e9d636275
Melissa Carbone <mcarbone1@gmail.com>
2013-12-22 14:24:20 -0500

    add validation for rating

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ActiveRecord::Base
   validates_presence_of :title
 
+  validates :rating, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,4 +4,7 @@ describe Book do
   it { should have_valid(:title).when('a title') }
   it { should_not have_valid(:title).when(nil, ' ') }
 
+  it { should have_valid(:rating).when(0, 50, 100) }
+  it { should_not have_valid(:rating).when(-1, 200, 101) }
+
 end
